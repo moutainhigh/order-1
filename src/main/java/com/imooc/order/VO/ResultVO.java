422d2225bf8c59471fdba398673a5ae3464fee0d
linzhuozhang <1109391515@qq.com>
2018-08-29 19:38:33 +0800

    order修改

---
+++ src/main/java/com/imooc/order/VO/ResultVO.java
@@ -0,0 +1,19 @@
+package com.imooc.order.VO;
+
+import lombok.Data;
+
+@Data
+public class ResultVO<T> {
+    /**
+     * 错误码
+     */
+    private Integer code;
+    /**
+     * 提示信息
+     */
+    private String msg;
+    /**
+     * 具体信息
+     */
+    private T data;
+}
