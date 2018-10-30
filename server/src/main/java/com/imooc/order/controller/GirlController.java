d2bfbd9213d534959f76d1b978cf76abe9f9bd83
linzhuozhang <1109391515@qq.com>
2018-10-30 20:57:53 +0800

    利用Spring Cloud Bus实现动态刷新配置

--- server/src/main/java/com/imooc/order/controller/GirlController.java
+++ server/src/main/java/com/imooc/order/controller/GirlController.java
@@ -1,22 +1,20 @@
 package com.imooc.order.controller;
 
-import org.springframework.beans.factory.annotation.Value;
+import com.imooc.order.config.GirlConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/env")
 @RefreshScope
-public class EnvController {
+public class GirlController {
 
-    @Value("${env}")
-    private String env;
+    @Autowired
+    private GirlConfig config;
 
-    @GetMapping("/print")
+    @GetMapping("/girl/print")
     public String print(){
-        return env;
+        return "name:" + config.getName() + ",age:" + config.getAge();
     }
-
 }
