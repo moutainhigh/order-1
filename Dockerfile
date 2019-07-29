0861ff3accc77f2d9260804c76e67858e7a5b4d5
linzhuozhang <1109391515@qq.com>
2019-07-29 10:52:25 +0800

    修改dockerfile文件

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,6 @@ MAINTAINER zoomzlin 1109391515@qq.com
 
 ADD server/target/*.jar app.jar
 
-EXPOSE 8080
+EXPOSE 8081
 
 ENTRYPOINT ["java", "-jar", "/app.jar"]
--- server/src/main/java/com/imooc/order/OrderApplication.java
+++ server/src/main/java/com/imooc/order/OrderApplication.java
@@ -3,6 +3,7 @@ package com.imooc.order;
 import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
